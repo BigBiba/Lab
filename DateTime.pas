7e0a8305bfd0450a056c43ca00c506ac461e28f4
Никита Федоров <nikita_fed@rambler.ru>
2022-10-06 14:53:11 +0300

    add close day

--- DateTime.pas
+++ DateTime.pas
@@ -1,7 +1,16 @@
 ﻿begin
   var year := DateTime.Now.Year;
   if (year mod 4 = 0) and (year mod 100 <> 0) then
-    Print($'{year} - високосный')
+    Println($'{year} - високосный')
   else
-    Print($'{year} - не високосный')
+    Println($'{year} - не високосный');
+  
+  var minute := DateTime.Now.Minute;
+  Print($'В {minute} минутах {minute * 60} секунд');
+  
+  var (a, b) := (ReadInteger, ReadInteger);
+  if (a mod 365) > (b mod 365) then
+    Println($'{a} ближе')
+  else
+    Println($'{b} ближе');
 end.
