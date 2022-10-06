4abb2ec364bc26e935d1cd50a6647d02d8d44b8c
Никита Федоров <nikita_fed@rambler.ru>
2022-10-06 14:27:05 +0300

    add get second

--- DateTime.pas
+++ DateTime.pas
@@ -1,7 +1,10 @@
 ﻿begin
   var year := DateTime.Now.Year;
   if (year mod 4 = 0) and (year mod 100 <> 0) then
-    Print($'{year} - високосный')
+    Println($'{year} - високосный')
   else
-    Print($'{year} - не високосный')
+    Println($'{year} - не високосный');
+  
+  var minute := DateTime.Now.Minute;
+  Print($'В {minute} минутах {minute * 60} секунд');
 end.
